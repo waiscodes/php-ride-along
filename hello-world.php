ab445bc42b2d724465258a96de2ef5b3619425ff
waiscodes <b.wais@outlook.com>
2020-09-30 11:06:05 -0600

    data types, and called

--- hello-world.php
+++ hello-world.php
@@ -14,4 +14,44 @@ echo "\n\n This is the difference between double quotes and single quotes. Doubl
 
 
 echo "\n \n this is how you " . 'concatenate a string!';
-echo "\n \n this solves the issue of having an overloaded value because"
+echo "\n \n this solves the issue of having an overloaded value because";
+
+// Data types
+
+$strings1 = 'I\'m a string';
+$strings2 = "double quote strings";
+
+//ints
+$int1 = 35;
+$int2 = 0;
+$int3 = -24;
+
+// Floats
+$floats1 = 3.14;
+$floats2 = -1.62;
+
+//Booleans
+$bool1 = true;
+$bool2 = False;
+
+// Arrays
+$array1 = ['First value', 17, 16.78, false]; // the data type doesn't matter
+$array2 = array('First value', 17, 16.78, false);
+
+//to output 17
+echo $array1[1];
+
+// Associative array 
+$array3 = array(
+    'key' => 'value',
+    'name' => 'George', 
+    'age' => 36
+);
+
+//to output name
+echo $array3['name'];
+
+
+
+
+?>

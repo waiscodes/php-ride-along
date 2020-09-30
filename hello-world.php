134ae4bb4d379bd51db1d4b82848f529a0b95841
waiscodes <b.wais@outlook.com>
2020-09-30 10:24:02 -0600

    created and called variables and used new line characters

--- hello-world.php
+++ hello-world.php
@@ -1,3 +1,10 @@
 <?php echo "hello, world!";
 //closing is optional if it's the end of the file
 //capitalizations on functions don't matter
+
+//Variable names start iwth dollar signs
+// Variables cannot start with numbers or special characters.
+// You need to check 
+$firstVariable = "\nhello world\n";
+echo $firstVariable;
+

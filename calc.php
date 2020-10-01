aa761b33429a8db14ce16dc74161b6348ed9ff74
waiscodes <b.wais@outlook.com>
2020-10-01 11:59:30 -0600

    created a calculator input field

--- calc.php
+++ calc.php
@@ -10,6 +10,24 @@ $GLOBALS['pageTitle'] = 'Calculator';
   Welcome to our Calculator Page
 </p>
 
+<form action="">
+  <label for="op1">Enter your first Operand</label>
+  <input type="number" name="value1" id="op1" value="">
+
+  <label for="operator"></label>
+  <select name="operator" id="operator">
+    <option value="add">+</option>
+    <option value="subtract">-</option>
+    <option value="multiply">&times;</option>
+    <option value="divide">&divide;</option>
+  </select>
+
+  <label for="op2">Enter your second Operand</label>
+  <input type="number" name="value2" id="op2" value="">
+
+  <input type="submit" value="Calculate">
+</form>
+
 <?php 
     //Footer
     include './templates/footer.php'

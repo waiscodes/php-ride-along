b3d82eb431069a2848246b95e93d6ea8bba2fe63
waiscodes <b.wais@outlook.com>
2020-10-01 11:31:15 -0600

    GLOBALS variable and associative arrays

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+
+// global $pageTitle; this is one way of making global variables but global variables are actually just a global array. It is easier to visualize doing it this way. 
+//This is an associative array.
+$GLOBALS['pageTitle'] = 'Home';
     //Header
     include './templates/header.php';
 ?>

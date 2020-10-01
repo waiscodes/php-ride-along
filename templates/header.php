5acd064fddf92e1464d3c069c501d97c5b49bd54
waiscodes <b.wais@outlook.com>
2020-10-01 10:53:09 -0600

    two different types of paths

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="./css/main.css">
+    <!-- Scripts -->
+    <script src="./js/scripts.js" defer></script>
+    <!-- ./ and just starting with the file stystem are the same -->
+</head>
+<body>
+    

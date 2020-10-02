012cd62390fa9a7e64bade6ead3f747f61444e82
waiscodes <b.wais@outlook.com>
2020-10-02 13:28:16 -0600

    imported class to page

--- snacks.php
+++ snacks.php
@@ -1,7 +1,10 @@
 <?php 
   $GLOBALS['pageTitle'] = 'Snacks Object Practice';
-  include './templates/header.php' ?>
+  // require_once only requires it once. And once it's already on that page, it doesn't require it again. If it required twice, it would break because you can't define a class twice. 
+  require_once './includes/Snacks.Class.php';
+  // the difference between require and include is, require throws an error and crashes the program if the file isn't there where as includes throws a warning in debugging. 
+  include './templates/header.php'; ?>
 
 
 
-<?php include './templates/footer.php' ?>
+<?php include './templates/footer.php'; ?>

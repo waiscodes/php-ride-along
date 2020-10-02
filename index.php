f413ac805ce9c26993cb4eb71f3938a8a0bd5526
waiscodes <b.wais@outlook.com>
2020-10-02 10:38:14 -0600

    added session to index

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+//You need to session_start() to any pages you want to use it on. Usually you'd do page by page, unless you want to use it on every page then you would put it in your header. 
+session_start();
 
 // global $pageTitle; this is one way of making global variables but global variables are actually just a global array. It is easier to visualize doing it this way. 
 //This is an associative array.
@@ -6,10 +8,18 @@ $GLOBALS['pageTitle'] = 'Home';
     //Header
     include './templates/header.php';
 ?>
-    
 
 
-
+<?php if (isset($_SESSION['calcHistory'])) : ?>
+    <h2>Calculator History from Session</h2>
+    <ul>
+        <?php foreach ($_SESSION['calcHistory'] as $calculation) : ?>
+            <li>
+                <?php echo $calculation; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif ?>
 <?php 
     //Footer
     include './templates/footer.php'

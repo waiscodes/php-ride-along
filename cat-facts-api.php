749859ce1709b17995a4392f1cff0f3886947f89
waiscodes <b.wais@outlook.com>
2020-10-05 11:06:28 -0600

    decoded json and added printed output onto the dom

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -1,8 +1,22 @@
 <?php
-$GLOBALS['pageTitle'] = 'Cat Facts Page';
-    //Header
-    include './templates/header.php';
+  $GLOBALS['pageTitle'] = 'Cat Facts Page';
+  include './templates/header.php';
 ?>
 
+<?php
+  // file_get_content gets documents from an external source, like that json file for example.
+  $dailyCatFactResponse = file_get_contents(
+    'https://cat-fact.herokuapp.com/facts/random'
+  );
+  
+  if ($dailyCatFactResponse)
+  {
+    $dailyCatFact = json_decode($dailyCatFactResponse);
+    ?>
+      <h2>Today's Cat Fact: </h2>
+      <p><?php echo $dailyCatFact->text; ?></p>
+    <?php
+  }
+?>
 
 <?php include './templates/footer.php' ?>

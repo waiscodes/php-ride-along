e2364b1880a8a8c96c63bef4b26d00b481cf8227
waiscodes <b.wais@outlook.com>
2020-10-06 10:22:10 -0600

    Added usestate and updated it through event handler

--- js/react-example.js
+++ js/react-example.js
@@ -1,15 +1,25 @@
 const reactRoot = document.querySelector("#react-root");
 
 const SearchForm = (props) => {
+  const [search, setSearch] = React.useState("");
+
   return (
     <React.Fragment>
       <form>
-        <label htmlFor="search">Search Snacks</label>
-        <input type="search" name="search" id="search" />
+        <label htmlFor="search">Search Snacks: </label>
+        <input
+          type="search"
+          name="search"
+          id="search"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          value={search}
+        />
         <input type="submit" value="Search Snack" />
       </form>
       <h3>Snack Search Results</h3>
-      <ul></ul>
+      <ul>{search}</ul>
     </React.Fragment>
   );
 };

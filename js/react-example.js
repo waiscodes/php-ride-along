c7f28171f384c6dc80a55d949074b461178a2c36
waiscodes <b.wais@outlook.com>
2020-10-06 10:14:29 -0600

    created a form using jsx

--- js/react-example.js
+++ js/react-example.js
@@ -1,3 +1,17 @@
 const reactRoot = document.querySelector("#react-root");
 
-ReactDOM.render(<p>hello world</p>, reactRoot);
+const SearchForm = (props) => {
+  return (
+    <React.Fragment>
+      <form>
+        <label htmlFor="search">Search Snacks</label>
+        <input type="search" name="search" id="search" />
+        <input type="submit" value="Search Snack" />
+      </form>
+      <h3>Snack Search Results</h3>
+      <ul></ul>
+    </React.Fragment>
+  );
+};
+
+ReactDOM.render(<SearchForm />, reactRoot);

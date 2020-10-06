5a6f59c04239382bf5bc8e4efe28b6fdf46c2037
waiscodes <b.wais@outlook.com>
2020-10-06 11:11:27 -0600

    mapped array onto the dom

--- js/react-example.js
+++ js/react-example.js
@@ -2,9 +2,20 @@ const reactRoot = document.querySelector("#react-root");
 
 const SearchForm = (props) => {
   const [search, setSearch] = React.useState("");
+  const [snacks, setSnacks] = React.useState([]);
 
   const submitSearch = (e) => {
     e.preventDefault();
+
+    fetch(`http://localhost:3000/api/snacks.php?search=${search}`)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setSnacks(result);
+      })
+      .catch((error) => {
+        throw error;
+      });
   };
   return (
     <React.Fragment>
@@ -22,7 +33,21 @@ const SearchForm = (props) => {
         <input type="submit" value="Search Snack" />
       </form>
       <h3>Snack Search Results</h3>
-      <ul></ul>
+      <ul>
+        {snacks.map((snack) => (
+          <li>
+            <h4>{snack[0]}</h4>
+            <dl>
+              <dt>Snack Type:</dt>
+              <dd>{snack[1]}</dd>
+              <dt>Snack Price:</dt>
+              <dd>${parseFloat(snack[2]).toFixed(2)}</dd>
+              <dt>Snack Calories:</dt>
+              <dd>{snack[3]}</dd>
+            </dl>
+          </li>
+        ))}
+      </ul>
     </React.Fragment>
   );
 };
